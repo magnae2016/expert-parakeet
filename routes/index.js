09df9314488ea168e43ce0f122f1c2954479d46e
magnae2016 <magnae2016@gmail.com>
2021-08-11 04:11:23 +0900

    Add detail

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get("/", function (req, res, next) {
     res.render("index", { title: "Express" });
 });
 
+/* GET home page. */
+router.get("/a", function (req, res, next) {
+    res.render("a", { title: "Express", menu: "a" });
+});
+
+/* GET home page. */
+router.get("/b", function (req, res, next) {
+    res.render("b", { title: "Express", menu: "b" });
+});
+
+/* GET home page. */
+router.get("/c", function (req, res, next) {
+    res.render("c", { title: "Express", menu: "c" });
+});
+
+/* GET home page. */
+router.get("/d", function (req, res, next) {
+    res.render("d", { title: "Express", menu: "d" });
+});
+
+/* GET home page. */
+router.get("/e", function (req, res, next) {
+    res.render("e", { title: "Express", menu: "e" });
+});
+
 module.exports = router;
